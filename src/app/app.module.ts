3a072e32d1f219b5940c0d5adabf24b45dd3b93a
Max Schulte <max.schulte@thinktecture.com>
2022-08-15 14:34:09 +0200

    chore: scaffolding

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,20 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { MainGroupComponent } from './main-group/main-group.component';
+import { ColumnComponent } from './column/column.component';
+import { RowComponent } from './row/row.component';
+import { SeperatorComponent } from './seperator/seperator.component';
+import { HeaderComponent } from './header/header.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MainGroupComponent,
+    ColumnComponent,
+    RowComponent,
+    SeperatorComponent,
+    HeaderComponent
   ],
   imports: [
     BrowserModule,
