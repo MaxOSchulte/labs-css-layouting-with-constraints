7fd9dd064b6fc94dbf3f982b3186d1384795d227
Max Schulte <max.schulte@thinktecture.com>
2022-08-18 11:48:18 +0200

    fields respect max grid size and inline group sizing on breakpoints

--- src/app/mock-field-group/mock-field-group.component.ts
+++ src/app/mock-field-group/mock-field-group.component.ts
@@ -7,4 +7,13 @@ import { CellDirective } from '../cell.directive';
   styleUrls: ['./mock-field-group.component.scss'],
 })
 export class MockFieldGroupComponent extends CellDirective {
+  @HostBinding('style.--inline-group-columns')
+  @Input() columns?: number | string;
+
+  @Input() useSpan?: boolean | '';
+
+  @HostBinding('style.--fallback-sizing')
+  private get fallbackColumns(): number | string | undefined {
+    return this.useSpan || this.useSpan === '' ? '--columns-span' : undefined;
+  }
 }
