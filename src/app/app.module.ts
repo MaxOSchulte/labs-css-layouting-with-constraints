7a5ae293eed8f18cfdeb1de48df65679a4295752
Max Schulte <max.schulte@thinktecture.com>
2022-09-25 08:27:38 +0200

    wip: no use: flex

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { SubGroupComponent } from './components/sub-group/sub-group.component';
+import { ItemComponent } from './components/item/item.component';
+import { GroupComponent } from './components/group/group.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SubGroupComponent,
+    ItemComponent,
+    GroupComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/components/sub-group/sub-group.component.ts
@@ -0,0 +1,8 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-sub-group',
+  templateUrl: './sub-group.component.html',
+  styleUrls: ['./sub-group.component.scss']
+})
+export class SubGroupComponent {}
